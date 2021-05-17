e1c59237c78b6635a2a1c3b849953c392943fbce
Pierre Spring <pierre.spring@caillou.ch>
2021-05-17 13:24:04 +0200

    Preload image over the fold

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ export default function Home() {
           src="/pierre-hq.jpg"
           layout="fill"
           objectFit="cover"
+          priority={true}
         />
       </div>
       <div className="fill-viewport"></div>
