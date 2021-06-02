ef62901799ed5ce6f0d7c65e67d54a2210aa55e0
Pierre Spring <pierre.spring@caillou.ch>
2021-06-02 10:42:42 +0200

    render images in parallel

--- scripts/renditions.ts
+++ scripts/renditions.ts
@@ -8,27 +8,27 @@ const imagesDirectory = join(__dirname, '../public');
 (async function () {
   await Promise.all(
     Object.entries(renditions).map(async ([fileName, configs]) => {
-      for (let index = 0; index < configs.length; index++) {
-        const config = configs[index];
-        if (!config) return;
-        for (let index = 0; index < config.sizes.length; index++) {
-          const size = config.sizes[index];
-          if (!size) return;
-          console.log(size, config.aspectRatio);
-          const buffer = await sharp(join(imagesDirectory, fileName))
-            .resize(size, Math.round(size / config.aspectRatio))
-            .toFormat('jpeg')
-            .toBuffer();
-          await writeFile(
-            join(
-              imagesDirectory,
-              'renditions',
-              `${fileName}-${size}@${config.aspectRatio}.jpeg`
-            ),
-            buffer
+      return Promise.all(
+        configs.map(async (config) => {
+          return Promise.all(
+            config.sizes.map(async (size) => {
+              console.log(size, config.aspectRatio);
+              const buffer = await sharp(join(imagesDirectory, fileName))
+                .resize(size, Math.round(size / config.aspectRatio))
+                .toFormat('jpeg')
+                .toBuffer();
+              await writeFile(
+                join(
+                  imagesDirectory,
+                  'renditions',
+                  `${fileName}-${size}@${config.aspectRatio}.jpeg`
+                ),
+                buffer
+              );
+            })
           );
-        }
-      }
+        })
+      );
     })
   );
 })();
