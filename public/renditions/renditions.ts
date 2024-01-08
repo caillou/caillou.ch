7f6edb5cd01289b1eb59ceedca16b3a33bd238a2
Pierre Spring <pierre.spring@caillou.ch>
2024-01-08 10:45:17 +0100

    💣 nuke existing page

--- public/renditions/renditions.ts
+++
@@ -1,7 +0,0 @@
-const sizes = [300, 600, 800, 1200, 1600, 2000, 2400, 3000, 4000, 5000];
-
-export const renditions = {
-  'pierre-hq.jpg': [0.25, 0.5, 1, 1.25, 1.5, 1.75, 2, 2.5].map(
-    (aspectRatio) => ({ aspectRatio, sizes })
-  ),
-};
